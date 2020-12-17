5eb2c05209c8acf6a1d766cecccaae9549701392
dl-watson <dlarkinwatson@gmail.com>
2020-12-17 13:48:31 -0800

    hooking up index.pug

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const app = require('./lib/app');
-const pool = require('./lib/utils/pool');
+/* eslint-disable no-console */
+const app = require("./lib/app");
+const pool = require("./lib/utils/pool");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 7890;
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+http.listen(PORT, () => {
   console.log(`Started on ${PORT}`);
 });
 
-process.on('exit', () => {
-  console.log('Goodbye!');
+process.on("exit", () => {
+  console.log("Goodbye!");
   pool.end();
 });
