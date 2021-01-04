8d6f37b1964a42f4b1c51ffa8551e6480df0f875
dl-watson <dlarkinwatson@gmail.com>
2021-01-03 17:58:25 -0800

    minor changes

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ const io = require("socket.io")(
 
 const Message = require("./lib/models/Message");
 
-setInterval(() => {
-  io.emit("data", "random number: " + Math.random().toString());
-}, 1000);
+// setInterval(() => {
+//   io.emit("data", "random number: " + Math.random().toString());
+// }, 1000);
 
 io.on("connection", (socket) => {
   console.log("a user connected");
--- views/js/chat_behavior.js
+++ views/js/chat_behavior.js
@@ -10,14 +10,14 @@ const chatBehavior = () => {
 
 const newMessage = (message) => {
   const new_message = document.createElement("li");
-  // const avatar = document.createElement("div");
+  const avatar = document.createElement("div");
   const text_wrapper = document.createElement("div");
   const text = document.createElement("div");
 
   new_message.classList.add("message");
 
-  // avatar.classList.add("avatar");
-  // avatar.textContent = message.name.toLowerCase();
+  avatar.classList.add("avatar");
+  avatar.textContent = message.name.toLowerCase();
 
   text_wrapper.classList.add("text_wrapper");
 
@@ -25,7 +25,7 @@ const newMessage = (message) => {
   text.textContent = message.chat_message;
 
   text_wrapper.appendChild(text);
-  new_message.append(/*avatar,*/ text_wrapper);
+  new_message.append(avatar, text_wrapper);
 
   document.querySelector(".messages").append(new_message);
 
